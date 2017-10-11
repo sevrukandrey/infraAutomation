b8707c1068580112a5ca900a1e7887a101d64f4e
Andrey Sevruk <asevruk@playtika.com>
2017-10-11 11:52:32 +0300

    1

--- src/com/playtika/fourth/hw/StreamsInServiceCode.java
+++ src/com/playtika/fourth/hw/StreamsInServiceCode.java
@@ -1,4 +1,43 @@
 package com.playtika.fourth.hw;
 
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class StreamsInServiceCode {
+
+
+    //Good
+//             private List<MysteryBonusInfo> removeFirstBonus(MysteryBonusesDocument mysteryBonusesDocument) {
+//                return mysteryBonusesDocument
+//            .availableToPlayBonuses
+//            .stream()
+//            .skip(1)
+//            .collect(toList());
+//            }
+
+    //----------------------------------------------------------------------------------//
+
+    //Code can be modify
+//    private final List<MegaBonusSegment> wedges;
+//
+//    public MegaBonusSegment getBySegmentId(int segmentId) {
+//
+//        for (MegaBonusSegment wedge : wedges) {
+//            if (wedge.getSegmentId() == segmentId) {
+//                return wedge;
+//            }
+//        }
+//        return null;
+//    }
+
+
+    //To
+//  public MegaBonusSegment getBySegmentId(int segmentId){
+//    return wedges.stream()
+//    .filter(w->w.getSegmentId()==segmentId)
+//    .findFirst().orElse(null);
+//    }
+
+//----------------------------------------------------------------------------------------//
 }
